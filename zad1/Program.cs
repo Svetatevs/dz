742589b9fb3b21d3fcee215f0bbe1c9ec985493a
Svetatevs <urusel.k@mail.ru>
2022-08-22 14:29:05 +0300

    initial commit

---
+++ zad1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2111111Hello, World!");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
